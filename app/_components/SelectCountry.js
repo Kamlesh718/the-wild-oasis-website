bfea892d5909e5922ecf74b72c46f9b69b9ced69
Kamlesh Vishwakarma <kamleshvishwakarma1202@gmail.com>
2024-05-24 18:05:23 +0530

    Initial Commit

---
+++ app/_components/SelectCountry.js
@@ -0,0 +1,33 @@
+import { getCountries } from "@/app/_lib/data-service";
+
+// Let's imagine your colleague already built this component 😃
+
+async function SelectCountry({ defaultCountry, name, id, className }) {
+  const countries = await getCountries();
+  // const countries = [];
+  const img =
+    "https://img.icons8.com/?size=100&id=32584&format=png&color=000000";
+
+  const flag =
+    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
+
+  return (
+    <select
+      name={name}
+      id={id}
+      // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
+      defaultValue={`${defaultCountry}%${flag}`}
+      className={className}
+    >
+      <option value="">Select country...</option>
+      {countries.map((c) => (
+        <option key={c.name} value={`${c.name}%${c.flag}`}>
+          {c.name}
+        </option>
+      ))}
+      {/* <option value={`India%${img}`}>India</option> */}
+    </select>
+  );
+}
+
+export default SelectCountry;
